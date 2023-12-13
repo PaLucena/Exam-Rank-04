f17552c51191cef106d937c0011e9a4fcacb273c
palucena <palucena@student.42malaga.com>
2023-12-13 18:48:39 +0100

    main done

--- microshell/microshell.c
+++ microshell/microshell.c
@@ -5,6 +5,23 @@
 #include <string.h>
 #include <sys/wait.h>
 
+int	ft_error(char *str)
+{
+	while (str)
+		write(2, *str++, 1);
+	return (1);
+}
+
+int	ft_cd(char **av, int i)
+{
+
+}
+
+int	ft_exec(char **av, char **envp, int i)
+{
+
+}
+
 int	main(int ac, char **av, char **envp)
 {
 	int i = 0;
@@ -14,7 +31,15 @@ int	main(int ac, char **av, char **envp)
 	{
 		while (av[i] && av[++i])
 		{
-			//cosas
+			av += i;
+			i = 0;
+			while (strcmp(av[i], "|") && strcmp(av[i], ";")
+				i++;
+			if (!strcmp(*av, "cd"))
+				status = ft_cd(av, i);
+			else if (i)
+				status = ft_exec(av, envp, i);
 		}
 	}
+	return (status);
 }
